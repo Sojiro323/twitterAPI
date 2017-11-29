772b526e88c2ebe05e8789edd702b936527cd076
Sojiro323 <m23622059@gmail.com>
2017-11-29 23:09:17 +0900

    pickle2database

--- mymodule/Mydatabase.py
+++ mymodule/Mydatabase.py
@@ -3,59 +3,48 @@
 import MySQLdb
 import sys,os
 
-DBNAME = 'twitter'
+'''
+# テーブルの作成
+sql = 'create table test (id int, content varchar(32))'
+c.execute(sql)
+print('* testテーブルを作成\n')
+
+# テーブル一覧の取得
+sql = 'show tables'
+c.execute(sql)
+print('===== テーブル一覧 =====')
+print(c.fetchone())
+
+# レコードの削除
+sql = 'delete from test where id=%s'
+c.execute(sql, (2,))
+print('\n* idが2のレコードを削除\n')
+
+# レコードの取得
+sql = 'select * from test'
+c.execute(sql)
+print('===== レコード =====')
+for row in c.fetchall():
+    print('Id:', row[0], 'Content:', row[1])
+'''
 
 
-def main():
+
+
+def insert(database, values):
+    DBNAME = 'twitterDB'
     conn = MySQLdb.connect(user="root",host="localhost",password="23622059",db=DBNAME)
     c = conn.cursor()
 
-    # テーブルの作成
-    sql = 'create table test (id int, content varchar(32))'
-    c.execute(sql)
-    print('* testテーブルを作成\n')
-
-    # テーブル一覧の取得
-    sql = 'show tables'
-    c.execute(sql)
-    print('===== テーブル一覧 =====')
-    print(c.fetchone())
-
     # レコードの登録
-    sql = 'insert into test values (%s, %s)'
-    c.execute(sql, (1, 'hoge'))  # 1件のみ
-    datas = [
-        (2, 'foo'),
-        (3, 'bar')
-    ]
-    c.executemany(sql, datas)    # 複数件
-    print('\n* レコードを3件登録\n')
-
-    # レコードの取得
-    sql = 'select * from test'
-    c.execute(sql)
-    print('===== レコード =====')
-    for row in c.fetchall():
-        print('Id:', row[0], 'Content:', row[1])
-
-    # レコードの削除
-    sql = 'delete from test where id=%s'
-    c.execute(sql, (2,))
-    print('\n* idが2のレコードを削除\n')
-
-    # レコードの取得
-    sql = 'select * from test'
-    c.execute(sql)
-    print('===== レコード =====')
-    for row in c.fetchall():
-        print('Id:', row[0], 'Content:', row[1])
+    if database == "follow_graph": sql = 'insert ignore into follow_graph values (%s, %s)'
+    else: sql = 'insert ignore into checked_list values (%s, %s, %s)'
+    if type(values) == 'tuple': c.execute(sql, values)  # 1件のみ
+    else: c.executemany(sql, values)    # 複数件
+    print('\n* complete : insert\n')
 
     # データベースへの変更を保存
     conn.commit()
 
     c.close()
     conn.close()
-
-
-if __name__ == '__main__':
-    main()
--- mymodule/Mypath.py
+++ mymodule/Mypath.py
@@ -4,7 +4,8 @@ import sys
 path_pattern = ["1","2","3","4","5","6","mutual","3_3a","3_3b","3_35","3_36","3_4a","3_4b","3_45","3_46","3_5a","3_5a","3_5b","3_6a","3_6b","4_123","4_124","4_125","4_126","4_3456","4_35a","4_35b","4_36a","4_36b","4_45a","4_45b","4_46a","4_46b","5_1235","5_1236","5_1245","5_1246","5_13456","5_23456","all"]
 
 
-path = sys.argv[1]
+#path = sys.argv[1]
+path = "./pickle/"
 load_files = Mypickle.load(path, ['friends_dic','followers_dic'])
 friends_dic = load_files[0]
 followers_dic = load_files[1]
@@ -37,9 +38,9 @@ def get_match():
       friends = friends_dic[seed]
       for friend in friends:
         if friend not in followers_dic: continue
-          temp = followers_dic[friend]
-          match_list = list(sert(match_list) & set(temp))
-          match_seeds = match(seed, match_list, match_seeds)
+        temp = followers_dic[friend]
+        match_list = list(sert(match_list) & set(temp))
+        match_seeds = match(seed, match_list, match_seeds)
 
   elif pattern is path_pattern[3]:#com_follower
 
@@ -48,9 +49,9 @@ def get_match():
       followers = followers_dic[seed]
       for follower in followers:
         if follower not in friends_dic: continue
-          temp = friends_dic[follower]
-          match_list = list(set(match_list) & set(temp))
-          match_seeds = match(seed, match_list, match_seeds)
+        temp = friends_dic[follower]
+        match_list = list(set(match_list) & set(temp))
+        match_seeds = match(seed, match_list, match_seeds)
 
 
   elif pattern is path_pattern[4]:#friend_friend
